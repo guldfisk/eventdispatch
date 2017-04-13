2995209f201aeebb5a1c21d4995b6020685eed98
guldfisk <ce.guldfisk@gmail.com>
2017-04-13 22:07:55 +0200

    wauw

--- eventdispatch/dispatcher.py
+++ eventdispatch/dispatcher.py
@@ -1,39 +1,40 @@
 import itertools
 import weakref
-from weakmethodref import WeakMethodRef
-		
+
+from eventdispatch.weakmethodref import WeakMethodRef
+
 class _WeakSet(set):
 	def __init__(self):
 		def _remove(item, wself=weakref.ref(self)):
 			self = wself()
 			if self is not None:
-				self.discardRef(item)
+				self.discard_ref(item)
 		self._remove = _remove
-	def registerRef(self, item):
+	def register_ref(self, item):
 		if hasattr(item, '__self__'):
 			return WeakMethodRef(item, self._remove)
 		return weakref.ref(item, self._remove)
 	def __iter__(self):
-		for itemref in set(self):
-			item = itemref()
+		for item_ref in set(self):
+			item = item_ref()
 			if item is not None:
 				yield item
 	def add(self, item):
-		super(_WeakSet, self).add(self.registerRef(item))
-	def discardRef(self, item):
+		super(_WeakSet, self).add(self.register_ref(item))
+	def discard_ref(self, item):
 		super(_WeakSet, self).discard(item)
 	def discard(self, item):
-		super(_WeakSet, self).discard(self.registerRef(item))
+		super(_WeakSet, self).discard(self.register_ref(item))
 	def __str__(self):
 		return '_WeakSet(%s)' % super(_WeakSet, self).__str__()
 
 class DispatchSession(object):
 	def __init__(self):
-		self.signaleMap = {}
+		self.signal_map = {}
 	def connect(self, f, signal=None):
-		if not signal in self.signaleMap:
-			self.signaleMap[signal] = _WeakSet()
-		self.signaleMap[signal].add(f)
+		if not signal in self.signal_map:
+			self.signal_map[signal] = _WeakSet()
+		self.signal_map[signal].add(f)
 	def send(self, signal, **kwargs):
 		return [
 			pair 
@@ -42,23 +43,23 @@ class DispatchSession(object):
 				(connected, connected(**kwargs))
 				for connected in
 				itertools.chain(
-					self.getConnected(signal), 
-					self.getConnected(None)
+					self.get_connected(signal),
+					self.get_connected(None)
 				)
 			)
 			if pair[0]
 		]
-	def getConnected(self, signal):
-		return self.signaleMap.get(signal, _WeakSet())
+	def get_connected(self, signal):
+		return self.signal_map.get(signal, _WeakSet())
 	def disconnect(self, f, signal=None):
-		self.signaleMap[signal].discard(f)
+		self.signal_map[signal].discard(f)
 	def __str__(self):
 		return 'DispatchSession(%s)' % {
 			key: [
 				item
 				for item in
-				self.signaleMap[key]
+				self.signal_map[key]
 			]
 			for key in
-			self.signaleMap
+			self.signal_map
 		}
