08d33a8079eb814809a3d5ff26d8f34328900833
guldfisk <ce.guldfisk@gmail.com>
2018-02-06 11:14:49 +0100

    np

--- eventdispatch/dispatcher.py
+++ eventdispatch/dispatcher.py
@@ -1,3 +1,5 @@
+import typing as t
+
 import itertools
 import weakref
 
@@ -6,12 +8,12 @@ from eventdispatch.weakmethodref import WeakMethodRef
 class _WeakSet(set):
 	def __init__(self):
 		super(_WeakSet, self).__init__()
-		def _remove(item, wself=weakref.ref(self)):
-			self = wself()
+		def _remove(item, weak_self=weakref.ref(self)):
+			self = weak_self()
 			if self is not None:
-				self.discard_ref(item)
+				self._discard_ref(item)
 		self._remove = _remove
-	def register_ref(self, item):
+	def _register_ref(self, item):
 		if hasattr(item, '__self__'):
 			return WeakMethodRef(item, self._remove)
 		return weakref.ref(item, self._remove)
@@ -21,25 +23,25 @@ class _WeakSet(set):
 			if item is not None:
 				yield item
 	def add(self, item):
-		super(_WeakSet, self).add(self.register_ref(item))
-	def discard_ref(self, item):
+		super(_WeakSet, self).add(self._register_ref(item))
+	def _discard_ref(self, item):
 		super(_WeakSet, self).discard(item)
 	def discard(self, item):
-		super(_WeakSet, self).discard(self.register_ref(item))
+		super(_WeakSet, self).discard(self._register_ref(item))
 	def __str__(self):
 		return '_WeakSet(%s)' % super(_WeakSet, self).__str__()
 
 class DispatchSession(object):
 	def __init__(self):
-		self.signal_map = {}
-	def connect(self, f, signal=None):
+		self.signal_map = {} #type: t.Dict[t.Hashable, t.Set[t.Callable]]
+	def connect(self, f: t.Callable, signal: t.Hashable = None):
 		if not signal in self.signal_map:
 			self.signal_map[signal] = _WeakSet()
 		self.signal_map[signal].add(f)
-	def send(self, signal, **kwargs):
+	def send(self, signal, **kwargs) -> t.List[t.Tuple[t.Callable, t.Any]]:
 		return [
-			pair 
-			for pair in 
+			(_connected, value)
+			for _connected, value in 
 			(
 				(connected, connected(**kwargs))
 				for connected in
@@ -48,19 +50,22 @@ class DispatchSession(object):
 					self.get_connected(None)
 				)
 			)
-			if pair[1] is not None
+			if value is not None
 		]
-	def get_connected(self, signal):
+	def get_connected(self, signal) -> t.Set[t.Callable]:
 		return self.signal_map.get(signal, _WeakSet())
-	def disconnect(self, f, signal=None):
+	def disconnect(self, f, signal=None) -> None:
 		self.signal_map[signal].discard(f)
 	def __str__(self):
-		return 'DispatchSession(%s)' % {
-			key: [
-				item
-				for item in
-				self.signal_map[key]
-			]
-			for key in
-			self.signal_map
-		}
+		return '{}({})'.format(
+			self.__class__.__name__,
+			{
+				key: [
+					item
+					for item in
+					self.signal_map[key]
+				]
+				for key in
+				self.signal_map
+			}
+		)
--- eventdispatch/weakmethodref.py
+++ eventdispatch/weakmethodref.py
@@ -3,34 +3,37 @@ import weakref
 
 class WeakMethodRef(object):
 	def __init__(self, f, callback=None):
-		if callback:
-			c = (lambda o: callback(self))
-		else:
-			c = None
 		try:
-			self._obj = weakref.ref(f.__self__, c)
-			self._meth = weakref.ref(f.__func__)
+			self._obj = weakref.ref(
+				f.__self__,
+				(
+					None
+					if callback is None else
+					(lambda o: callback(self))
+				),
+			)
+			self._method = weakref.ref(f.__func__)
 			self.hash = hash(
 				types.MethodType(
-					self._meth(),
-					self._obj()
+					self._method(),
+					self._obj(),
 				)
 			)
 		except AttributeError:
 			self._obj = None
-			self._meth = None
+			self._method = None
 			self.hash = hash(None)
 	def __call__(self):
 		if not self.dead():
 			return types.MethodType(
-				self._meth(),
+				self._method(),
 				self._obj()
 			)
 	def __eq__(self, other):
 		return (
 			hasattr(other, '__self__')
-			and self._obj==other.__self__()
-			and self._meth==other.__func__()
+			and self._obj == other.__self__()
+			and self._method == other.__func__()
 		)
 	def __hash__(self):
 		return self.hash
@@ -39,14 +42,14 @@ class WeakMethodRef(object):
 	def __self__(self):
 		return self._obj
 	def __func__(self):
-		return self._meth
+		return self._method
 	def __str__(self):
 		if self.dead():
 			return '<%s; dead>' % super(WeakMethodRef, self).__repr__()
 		return '<%s; to %r' % (
 			super(WeakMethodRef, self).__repr__(),
 			types.MethodType(
-				self._meth(),
+				self._method(),
 				self._obj()
 			)
 		)
