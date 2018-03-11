e033c5e1d620895bdbf78ef00e5ea777a91ffe29
guldfisk <ce.guldfisk@gmail.com>
2018-03-11 16:16:15 +0100

    np

--- eventdispatch/dispatcher.py
+++ eventdispatch/dispatcher.py
@@ -6,6 +6,7 @@ import weakref
 from eventdispatch.weakmethodref import WeakMethodRef
 
 class WeakSet(set):
+
 	def __init__(self):
 		super(WeakSet, self).__init__()
 		def _remove(item, weak_self=weakref.ref(self)):
@@ -13,37 +14,44 @@ class WeakSet(set):
 			if self is not None:
 				self._discard_ref(item)
 		self._remove = _remove
+
 	def _register_ref(self, item):
 		if hasattr(item, '__self__'):
 			return WeakMethodRef(item, self._remove)
 		return weakref.ref(item, self._remove)
+
 	def __iter__(self):
 		for item_ref in set(self):
 			item = item_ref()
 			if item is not None:
 				yield item
+
 	def add(self, item):
 		super(WeakSet, self).add(self._register_ref(item))
+
 	def _discard_ref(self, item):
 		super(WeakSet, self).discard(item)
+
 	def discard(self, item):
 		super(WeakSet, self).discard(self._register_ref(item))
-	def __str__(self):
-		return '_WeakSet(%s)' % super(WeakSet, self).__str__()
+
 
 class DispatchSession(object):
+
 	def __init__(self):
 		self.signal_map = {} #type: t.Dict[t.Hashable, t.Set[t.Callable]]
+
 	def connect(self, f: t.Callable, signal: t.Hashable = None):
 		if not signal in self.signal_map:
 			self.signal_map[signal] = WeakSet()
 		self.signal_map[signal].add(f)
-	def send(self, signal, **kwargs) -> t.List[t.Tuple[t.Callable, t.Any]]:
+
+	def send(self, signal, source: t.Optional[t.Any] = None, **kwargs) -> t.List[t.Tuple[t.Callable, t.Any]]:
 		return [
 			(_connected, value)
 			for _connected, value in 
 			(
-				(connected, connected(**kwargs))
+				(connected, connected(source, **kwargs))
 				for connected in
 				itertools.chain(
 					self.get_connected(signal),
@@ -52,10 +60,13 @@ class DispatchSession(object):
 			)
 			if value is not None
 		]
+
 	def get_connected(self, signal) -> t.Set[t.Callable]:
 		return self.signal_map.get(signal, WeakSet())
+
 	def disconnect(self, f, signal=None) -> None:
 		self.signal_map[signal].discard(f)
+
 	def __str__(self):
 		return '{}({})'.format(
 			self.__class__.__name__,
