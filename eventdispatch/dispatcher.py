9e4220fa48556682cc804cd7b7ee8ba25f6171a6
guldfisk <ce.guldfisk@gmail.com>
2019-10-28 17:00:31 +0100

    ok

--- eventdispatch/dispatcher.py
+++ eventdispatch/dispatcher.py
@@ -5,78 +5,81 @@ import weakref
 
 from eventdispatch.weakmethodref import WeakMethodRef
 
+
 class WeakSet(set):
 
-	def __init__(self):
-		super(WeakSet, self).__init__()
-		def _remove(item, weak_self=weakref.ref(self)):
-			self = weak_self()
-			if self is not None:
-				self._discard_ref(item)
-		self._remove = _remove
+    def __init__(self):
+        super(WeakSet, self).__init__()
 
-	def _register_ref(self, item):
-		if hasattr(item, '__self__'):
-			return WeakMethodRef(item, self._remove)
-		return weakref.ref(item, self._remove)
+        def _remove(item, weak_self = weakref.ref(self)):
+            self = weak_self()
+            if self is not None:
+                self._discard_ref(item)
 
-	def __iter__(self):
-		for item_ref in set(self):
-			item = item_ref()
-			if item is not None:
-				yield item
+        self._remove = _remove
 
-	def add(self, item):
-		super(WeakSet, self).add(self._register_ref(item))
+    def _register_ref(self, item):
+        if hasattr(item, '__self__'):
+            return WeakMethodRef(item, self._remove)
+        return weakref.ref(item, self._remove)
 
-	def _discard_ref(self, item):
-		super(WeakSet, self).discard(item)
+    def __iter__(self):
+        for item_ref in set(self):
+            item = item_ref()
+            if item is not None:
+                yield item
 
-	def discard(self, item):
-		super(WeakSet, self).discard(self._register_ref(item))
+    def add(self, item):
+        super(WeakSet, self).add(self._register_ref(item))
+
+    def _discard_ref(self, item):
+        super(WeakSet, self).discard(item)
+
+    def discard(self, item):
+        super(WeakSet, self).discard(self._register_ref(item))
 
 
 class DispatchSession(object):
 
-	def __init__(self):
-		self.signal_map = {} #type: t.Dict[t.Hashable, t.Set[t.Callable]]
+    def __init__(self):
+        self.signal_map: t.Dict[t.Hashable, t.Set[t.Callable]] = {}
 
-	def connect(self, f: t.Callable, signal: t.Hashable = None):
-		if not signal in self.signal_map:
-			self.signal_map[signal] = WeakSet()
-		self.signal_map[signal].add(f)
+    def connect(self, f: t.Callable, signal: t.Hashable = None):
+        if not signal in self.signal_map:
+            self.signal_map[signal] = WeakSet()
+        self.signal_map[signal].add(f)
 
-	def send(self, signal, source: t.Optional[t.Any] = None, **kwargs) -> t.List[t.Tuple[t.Callable, t.Any]]:
-		return [
-			(_connected, value)
-			for _connected, value in 
-			(
-				(connected, connected(source, **kwargs))
-				for connected in
-				itertools.chain(
-					self.get_connected(signal),
-					self.get_connected(None)
-				)
-			)
-			if value is not None
-		]
+    def send(self, signal, source: t.Optional[t.Any] = None, **kwargs) -> t.List[t.Tuple[t.Callable, t.Any]]:
+        return [
+            (_connected, value)
+            for _connected, value in
+            (
+                (connected, connected(source, **kwargs))
+                for connected in
+                itertools.chain(
+                    self.get_connected(signal),
+                    self.get_connected(None)
+                )
+            )
+            if value is not None
+        ]
 
-	def get_connected(self, signal) -> t.Set[t.Callable]:
-		return self.signal_map.get(signal, WeakSet())
+    def get_connected(self, signal) -> t.Set[t.Callable]:
+        return self.signal_map.get(signal, WeakSet())
 
-	def disconnect(self, f, signal=None) -> None:
-		self.signal_map[signal].discard(f)
+    def disconnect(self, f, signal = None) -> None:
+        self.signal_map[signal].discard(f)
 
-	def __str__(self):
-		return '{}({})'.format(
-			self.__class__.__name__,
-			{
-				key: [
-					item
-					for item in
-					self.signal_map[key]
-				]
-				for key in
-				self.signal_map
-			}
-		)
+    def __str__(self):
+        return '{}({})'.format(
+            self.__class__.__name__,
+            {
+                key: [
+                    item
+                    for item in
+                    self.signal_map[key]
+                ]
+                for key in
+                self.signal_map
+            }
+        )
--- eventdispatch/weakmethodref.py
+++ eventdispatch/weakmethodref.py
@@ -1,55 +1,66 @@
 import types
 import weakref
 
+
 class WeakMethodRef(object):
-	def __init__(self, f, callback=None):
-		try:
-			self._obj = weakref.ref(
-				f.__self__,
-				(
-					None
-					if callback is None else
-					(lambda o: callback(self))
-				),
-			)
-			self._method = weakref.ref(f.__func__)
-			self.hash = hash(
-				types.MethodType(
-					self._method(),
-					self._obj(),
-				)
-			)
-		except AttributeError:
-			self._obj = None
-			self._method = None
-			self.hash = hash(None)
-	def __call__(self):
-		if not self.dead():
-			return types.MethodType(
-				self._method(),
-				self._obj()
-			)
-	def __eq__(self, other):
-		return (
-			hasattr(other, '__self__')
-			and self._obj == other.__self__()
-			and self._method == other.__func__()
-		)
-	def __hash__(self):
-		return self.hash
-	def dead(self):
-		return self._obj is None or self._obj() is None
-	def __self__(self):
-		return self._obj
-	def __func__(self):
-		return self._method
-	def __str__(self):
-		if self.dead():
-			return '<%s; dead>' % super(WeakMethodRef, self).__repr__()
-		return '<%s; to %r' % (
-			super(WeakMethodRef, self).__repr__(),
-			types.MethodType(
-				self._method(),
-				self._obj()
-			)
-		)
+
+    def __init__(self, f, callback = None):
+        try:
+            self._obj = weakref.ref(
+                f.__self__,
+                (
+                    None
+                    if callback is None else
+                    (lambda o: callback(self))
+                ),
+            )
+            self._method = weakref.ref(f.__func__)
+            self.hash = hash(
+                types.MethodType(
+                    self._method(),
+                    self._obj(),
+                )
+            )
+        except AttributeError:
+            self._obj = None
+            self._method = None
+            self.hash = hash(None)
+
+    def __call__(self):
+        if not self.dead():
+            return types.MethodType(
+                self._method(),
+                self._obj()
+            )
+
+    def __eq__(self, other):
+        return (
+            hasattr(other, '__self__')
+            and self._obj == other.__self__()
+            and self._method == other.__func__()
+        )
+
+    def __hash__(self):
+        return self.hash
+
+    def dead(self):
+        return self._obj is None or self._obj() is None
+
+    def __self__(self):
+        return self._obj
+
+    def __func__(self):
+        return self._method
+
+    def __str__(self):
+        if self.dead():
+            return '<{}; dead>'.format(
+                super(WeakMethodRef, self).__repr__()
+            )
+        return '<{}; to {}>'.format(
+            super(WeakMethodRef, self).__repr__(),
+            types.MethodType(
+                self._method(),
+                self._obj()
+            )
+        )
